b010198e57e794b1e73e0465435213e554bdbfc5
Hunter Madison <hmadison@users.noreply.github.com>
2021-01-18 10:07:59 -0500

    Bring over Rubocop, run suite on CI

--- lib/lobstersbot/plugins/rss.rb
+++ lib/lobstersbot/plugins/rss.rb
@@ -1,11 +1,11 @@
 module Lobstersbot
   module Rss
     def frequently_post_stories(
-      memory, 
+      memory,
       reader = RssReader.new('https://lobste.rs/newest.rss')
     )
       reader.call(memory[:last_run]).each do |story|
-        @config[:channels].each { |c| privmsg(story, c) }
+        @config[:channels].each {|c| privmsg(story, c) }
       end
       memory[:last_run] = Time.now.to_i
     end
--- spec/lobstersbot/rss_spec.rb
+++ spec/lobstersbot/rss_spec.rb
@@ -2,21 +2,20 @@ require 'spec_helper'
 
 RSpec.describe Lobstersbot::Rss do
   include described_class
-  
+
   it 'requests and posts stories' do
-    response = double()
-    memory = Hash.new
-    
-    @config = Hash.new
+    response = double
+    memory = {}
+
+    @config = {}
     @config[:channels] = ['test']
-    
-    def privmsg(msg, to)
-    end
-    
-    expect(response).to receive(:call).with(nil).and_return(['test'])
-    
+
+    def privmsg(msg, to); end
+
+    allow(response).to receive(:call).with(nil).and_return(['test'])
+
     frequently_post_stories(memory, response)
-    
+
     expect(memory[:last_run]).to_not be_nil
   end
 end
